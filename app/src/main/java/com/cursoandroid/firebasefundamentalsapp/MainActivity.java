6a18215bdb997c2a25354aefc9d7201ee1d00c71
Juliana Salafia <juliana.salafia1@gmail.com>
2022-06-11 21:18:34 -0300

    saving and updating data with firebase

--- app/src/main/java/com/cursoandroid/firebasefundamentalsapp/MainActivity.java
+++ app/src/main/java/com/cursoandroid/firebasefundamentalsapp/MainActivity.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //createUser();
+        createProduct();
+    }
+
+    private void createUser() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference users = reference.child("users");
+
+        User user = new User();
+        user.setName("Denis");
+        user.setSurname("Sato");
+        user.setAge(22);
+
+        users.child("002").setValue(user);
+    }
+
+    private void createProduct() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference products = reference.child("products");
+
+        Product product = new Product();
+        product.setDescription("Acer Aspire");
+        product.setBrand("Acer");
+        product.setPrice(999.99);
+
+        products.child("002").setValue(product);
     }
 }
--- app/src/main/java/com/cursoandroid/firebasefundamentalsapp/Product.java
+++ app/src/main/java/com/cursoandroid/firebasefundamentalsapp/Product.java
@@ -1,4 +1,35 @@
 package com.cursoandroid.firebasefundamentalsapp;
 
 public class Product {
+    private String description;
+    private String brand;
+    private Double price;
+
+    public Product() {
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
